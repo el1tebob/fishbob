fa3d6bc570f56e988c3b637237ebcd509c89a1d8
el1tebob <elitebob41@gmail.com>
2024-08-08 10:21:50 +0200

    go

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+let fish1 = 0
+let intervalId;
 
+document.getElementById('fishing').addEventListener('click', () => {
+    if (!intervalId) {
+        intervalId = setInterval(() => {
+            count++;
+            document.getElementById('counter').textContent = count;
+        }, 1000);
+    }
+});
